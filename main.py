a43919ade5061889b8460756e39a6a3bd67033be
promsm <dobmsm@gmail.com>
2025-11-08 15:19:09 +0900

    Updaet main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 import time
 import streamlit as st
 
 #load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+#api_key = os.getenv("OPENAI_API_KEY")
 
 #ChatOpenAI 생성
 llm = ChatOpenAI(api_key=api_key,model='gpt-4o-mini')
